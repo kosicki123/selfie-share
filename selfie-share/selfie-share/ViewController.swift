ce45cb8f50a0a54ecd3ee4459de5035a5420e47f
Renan Kosicki <renankosicki@gmail.com>
2015-09-27 21:13:24 -0300

    Project created

---
+++ selfie-share/selfie-share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfie-share
+//
+//  Created by Renan Kosicki on 9/27/15.
+//  Copyright © 2015 Renan Kosicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
